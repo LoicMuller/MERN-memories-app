44f65bb4d9b36c0542ba5746efa73ef7102c28d0
Loic Muller <loicdev069@gmail.com>
2021-03-23 18:28:06 +0100

    install packages

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECION_URL =
+  "mongodb+srv://loicmuller:loicmuller01@cluster0.4cveo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
