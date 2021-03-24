ee3d3e7147ef9211a5e832a860521e548c55197f
Loïc Muller <loicmuller2001@outlook.fr>
2021-03-24 01:28:01 +0100

    first step done

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECION_URL =
   "mongodb+srv://loicmuller:loicmuller01@cluster0.4cveo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
